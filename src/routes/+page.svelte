086037f0961eb08c7605ca6999a7dea6b7d85ec9
Anjali <holla2467@gmail.com>
2024-07-31 16:11:23 +0530

    CONFIG: tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-green-400">The Movie browser</h1>
+<div></div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
