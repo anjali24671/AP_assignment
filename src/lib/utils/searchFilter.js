cac7c82fc55cce14c5c500f03065e8511a03a7f5
Anjali <holla2467@gmail.com>
2024-07-31 21:31:17 +0530

    REFACT: change url

--- src/lib/utils/searchFilter.js
+++ src/lib/utils/searchFilter.js
@@ -2,9 +2,9 @@ import { TMDB_ACCESS_TOKEN } from '$env/static/private'
 
 
 // search movies with filters such as genre, release year range and rating
-export async function searchFilter(genreID, releaseYearStart, releaseYearEnd, rating) {
+export async function searchFilter(genreID, releaseYearStart, releaseYearEnd, rating, page=1) {
     try{
-        const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${genreID}&release_date.gte=${releaseYearStart}&release_date.lte=${releaseYearEnd}&vote_average.gte=${rating}`
+        const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${genreID}&release_date.gte=${releaseYearStart}&release_date.lte=${releaseYearEnd}&vote_average.gte=${rating}&page=${page}`
 
         const movieRes = await fetch(url, {
             method: 'GET',
--- src/lib/utils/searchMovies.js
+++ src/lib/utils/searchMovies.js
@@ -1,9 +1,10 @@
 import { TMDB_ACCESS_TOKEN } from '$env/static/private';
 
 // Searches movies by name
-export async function searchMovies(title) {
+export async function searchMovies(title, page=1) {
     try {
-        let url = ''
+        let url = `https://api.themoviedb.org/3/movie/popular?query=${title}&page=${page}`
+
         const movieRes = fetch(url, {
             method: 'GET',
             headers: {
