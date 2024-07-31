84ac7846ce700ff4c5130527086e6da76f67b5c4
Anjali <holla2467@gmail.com>
2024-07-31 19:54:25 +0530

    CONFIG: tailwindcss

---
+++ svelte.config.mjs
@@ -0,0 +1,12 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+
+export default {
+  preprocess: preprocess({
+    postcss: true,
+  }),
+
+  kit: {
+    adapter: adapter(),
+  },
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-// tailwind.config.js
 export default {
-  content: [
-    './src/**/*.{html,js,svelte,ts}'
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
