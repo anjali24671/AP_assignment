b4b3ca391fb66c7663d19454480c4c298ffce6ec
Anjali <holla2467@gmail.com>
2024-07-31 21:30:22 +0530

    Feat: save movies

---
+++ src/lib/component/SaveMovieComponent.svelte
@@ -0,0 +1,14 @@
+<div class="flex gap-4 max-w-[700px]">
+    <div class="w-[200px]">
+        <img src="src\lib\static\radhe.png">
+    </div>
+    <div class="flex ">
+        <div>
+            <h1 class="text-lg font-semibold">little radha: the darling of vrindavana</h1>
+            <h2>Action Drama Bhakti</h2>
+            <div>any other info</div>
+        </div>
+       
+        <button>remove</button>
+    </div>
+</div>
--- src/routes/saved_movies/+page.svelte
+++ src/routes/saved_movies/+page.svelte
@@ -1,4 +1,11 @@
-<div class="flex items-center flex-col justify-center">
+<script>
+    import SaveMovieComponent from '../../lib/component/SaveMovieComponent.svelte'
+</script>
+
+
+<div class="flex items-center gap-2 flex-col justify-center">
     <div>Saved Movies</div>
-    <div></div>
+    <SaveMovieComponent/>
+    <SaveMovieComponent/>
+    <SaveMovieComponent/>
 </div>
